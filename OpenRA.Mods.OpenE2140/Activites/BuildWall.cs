62002f4044ee5ba55738bbdc30fb0e178558afe9
michaeldgg2 <119738087+michaeldgg2@users.noreply.github.com>
2024-03-30 22:40:39 +0100

    Miner BT: WallBuilder trait and BuildWall activity for building walls (WIP)

---
+++ OpenRA.Mods.OpenE2140/Activites/BuildWall.cs
@@ -0,0 +1,100 @@
+﻿using OpenRA.Activities;
+using OpenRA.Mods.Common.Activities;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Mods.OpenE2140.Traits.Miner;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.OpenE2140.Activites;
+
+public class BuildWall : Activity
+{
+	private enum BuildState { None, MovingToTarget, Building }
+
+	private readonly CPos targetLocation;
+	private readonly WallBuilder wallBuilder;
+	private readonly Mobile mobile;
+	private readonly AmmoPool[] ammoPools;
+
+	private BuildState state = BuildState.None;
+
+	public BuildWall(Actor self, CPos targetLocation)
+	{
+		this.targetLocation = targetLocation;
+		this.wallBuilder = self.Trait<WallBuilder>();
+		this.mobile = self.Trait<Mobile>();
+		this.ammoPools = self.TraitsImplementing<AmmoPool>().Where(p => p.Info.Name == this.wallBuilder.Info.AmmoPoolName).ToArray();
+	}
+
+	public override bool Tick(Actor self)
+	{
+		if (this.IsCanceling)
+		{
+			return true;
+		}
+
+		switch (this.state)
+		{
+			case BuildState.None:
+			{
+				if (self.Location != this.targetLocation)
+				{
+					this.QueueChild(this.mobile.MoveTo(this.targetLocation));
+				}
+
+				this.state = BuildState.MovingToTarget;
+				break;
+			}
+			case BuildState.MovingToTarget:
+			{
+				if (self.Location != this.targetLocation)
+				{
+					this.QueueChild(this.mobile.MoveTo(this.targetLocation));
+				}
+				else
+				{
+					if (this.ammoPools != null)
+					{
+						var pool = this.ammoPools.FirstOrDefault();
+						if (pool == null)
+							return false;
+
+						if (pool.CurrentAmmoCount < this.wallBuilder.Info.AmmoUsage)
+							return false;
+					}
+
+					this.state = BuildState.Building;
+					this.QueueChild(new Wait(this.wallBuilder.Info.PreBuildDelay));
+				}
+				break;
+			}
+			case BuildState.Building:
+			{
+				if (this.ammoPools != null)
+				{
+					var pool = this.ammoPools.FirstOrDefault();
+					if (pool == null)
+						return false;
+
+					if (!pool.TakeAmmo(self, this.wallBuilder.Info.AmmoUsage))
+						return false;
+				}
+
+				this.wallBuilder.OnWallBuilt(self, this.targetLocation);
+				return true;
+			}
+			default:
+				break;
+		}
+		return false;
+	}
+
+	public override IEnumerable<TargetLineNode> TargetLineNodes(Actor self)
+	{
+		yield return new TargetLineNode(Target.FromCell(self.World, this.targetLocation), this.wallBuilder.Info.TargetLineColor);
+	}
+
+	public override IEnumerable<Target> GetTargets(Actor self)
+	{
+		yield return Target.FromCell(self.World, this.targetLocation);
+	}
+}
---
+++ OpenRA.Mods.OpenE2140/Traits/Miner/WallBuilder.cs
@@ -0,0 +1,75 @@
+﻿using OpenRA.Mods.Common.Traits;
+using OpenRA.Mods.OpenE2140.Activites;
+using OpenRA.Primitives;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.OpenE2140.Traits.Miner;
+
+public class WallBuilderInfo : ConditionalTraitInfo, Requires<MobileInfo>
+{
+	[ActorReference]
+	[Desc("Wall actor.")]
+	[FieldLoader.Require]
+	public readonly string Wall = "";
+
+	[Desc("Number of ticks it takes to build a wall.")]
+	public readonly int PreBuildDelay = 0;
+
+	[Desc("Color to use for the target line when building walls.")]
+	public readonly Color TargetLineColor = Color.Crimson;
+
+	[Desc("Name of the ammo pool the wall builder uses.")]
+	public readonly string AmmoPoolName = "primary";
+
+	[Desc("Ammo the wall builder consumes per wall node.")]
+	public readonly int AmmoUsage = 1;
+
+	public override object Create(ActorInitializer init)
+	{
+		return new WallBuilder(init.Self, this);
+	}
+}
+
+public class WallBuilder : ConditionalTrait<WallBuilderInfo>, IResolveOrder, ITick
+{
+	public const string BuildWallID = "BuildWall";
+
+	private CPos? newWallLocation;
+
+	public WallBuilder(Actor self, WallBuilderInfo info)
+		: base(info)
+	{
+	}
+
+	void IResolveOrder.ResolveOrder(Actor self, Order order)
+	{
+		if (order.OrderString != BuildWallID)
+			return;
+
+		self.QueueActivity(new BuildWall(self, self.World.Map.CellContaining(order.Target.CenterPosition)));
+	}
+
+	public void OnWallBuilt(Actor self, CPos wallLocation)
+	{
+		this.newWallLocation = wallLocation;
+	}
+
+	void ITick.Tick(Actor self)
+	{
+		if (this.newWallLocation != null && self.Location != this.newWallLocation)
+		{
+			self.World.AddFrameEndTask(w =>
+			{
+				if (!self.World.ActorMap.GetActorsAt(this.newWallLocation.Value).All(a => a == self)) return;
+
+				var mine = w.CreateActor(this.Info.Wall, new TypeDictionary
+				{
+					new LocationInit(this.newWallLocation.Value),
+					new OwnerInit(self.Owner)
+				});
+
+				this.newWallLocation = null;
+			});
+		}
+	}
+}
