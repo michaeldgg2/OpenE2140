c8716a68077ca72c68be93067589642aa425bc59
michaeldgg2 <119738087+michaeldgg2@users.noreply.github.com>
2024-03-30 22:40:39 +0100

    Miner BT: WallBuilder trait and BuildWall activity for building walls (WIP)

---
+++ OpenRA.Mods.OpenE2140/Traits/Miner/WallBuilder.cs
@@ -0,0 +1,117 @@
+﻿using OpenRA.Mods.Common.Traits;
+using OpenRA.Mods.OpenE2140.Activites;
+using OpenRA.Primitives;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.OpenE2140.Traits.Miner;
+
+public class WallBuilderInfo : ConditionalTraitInfo, Requires<MobileInfo>
+{
+	[ActorReference]
+	[Desc("Wall actor.")]
+	[FieldLoader.Require]
+	public readonly string Wall = "";
+
+	[VoiceReference]
+	[Desc("Voice to use when ordered build a wall.")]
+	public readonly string Voice = "Action";
+
+	[Desc("Number of ticks it takes to build a wall.")]
+	public readonly int PreBuildDelay = 0;
+
+	[Desc("Color to use for the target line when building walls.")]
+	public readonly Color TargetLineColor = Color.Crimson;
+
+	[Desc("Only allow building walls on listed terrain types. Leave empty to allow all terrain types.")]
+	public readonly HashSet<string> TerrainTypes = new();
+
+	[CursorReference]
+	[Desc("Cursor to display when able to build a wall.")]
+	public readonly string BuildCursor = "wallPlace";
+
+	[CursorReference]
+	[Desc("Cursor to display when unable to build a wall.")]
+	public readonly string BuildBlockedCursor = "generic-blocked";
+
+	[Desc("Name of the ammo pool the wall builder uses.")]
+	public readonly string AmmoPoolName = "primary";
+
+	[Desc("Ammo the wall builder consumes per wall node.")]
+	public readonly int AmmoUsage = 1;
+
+	public override object Create(ActorInitializer init)
+	{
+		return new WallBuilder(init.Self, this);
+	}
+}
+
+public class WallBuilder : ConditionalTrait<WallBuilderInfo>, IResolveOrder, ITick, IOrderVoice
+{
+	public const string BuildWallOrderID = "BuildWall";
+
+	private CPos? newWallLocation;
+
+	public WallBuilder(Actor self, WallBuilderInfo info)
+		: base(info)
+	{
+	}
+
+	void IResolveOrder.ResolveOrder(Actor self, Order order)
+	{
+		if (order.OrderString != BuildWallOrderID)
+			return;
+
+		var targetCell = self.World.Map.CellContaining(order.Target.CenterPosition);
+		if (!this.IsCellAcceptable(self, targetCell))
+			return;
+
+		self.QueueActivity(order.Queued, new BuildWall(self, targetCell));
+	}
+
+	public bool IsCellAcceptable(Actor self, CPos cell)
+	{
+		if (!self.World.Map.Contains(cell) || this.newWallLocation == cell)
+			return false;
+
+		if (this.Info.TerrainTypes.Count == 0)
+			return true;
+
+		var terrainType = self.World.Map.GetTerrainInfo(cell).Type;
+		return this.Info.TerrainTypes.Contains(terrainType);
+	}
+
+	public void OnWallBuilt(Actor self, CPos wallLocation)
+	{
+		this.newWallLocation = wallLocation;
+	}
+
+	void ITick.Tick(Actor self)
+	{
+		if (this.newWallLocation != null && self.Location != this.newWallLocation)
+		{
+			self.World.AddFrameEndTask(w =>
+			{
+				if (!self.World.ActorMap.GetActorsAt(this.newWallLocation.Value).All(a => a == self)) return;
+
+				var wall = w.CreateActor(this.Info.Wall, new TypeDictionary
+				{
+					new LocationInit(this.newWallLocation.Value),
+					new OwnerInit(self.Owner)
+				});
+
+				this.newWallLocation = null;
+
+				foreach (var t in self.TraitsImplementing<INotifyWallBuilding>())
+					t.WallCreated(self, wall);
+			});
+		}
+	}
+
+	string? IOrderVoice.VoicePhraseForOrder(Actor self, Order order)
+	{
+		if (order.OrderString == BuildWallOrderID)
+			return this.Info.Voice;
+
+		return null;
+	}
+}
